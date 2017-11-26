cadd197fce85be592a5b578758248c94134c359a
Yves Riedener <git@reecube.com>
2017-11-26 19:12:27 +0100

    removed unused code, kept base

--- src/ffhs/pa5/Constants.java
+++ src/ffhs/pa5/Constants.java
@@ -8,23 +8,17 @@ package ffhs.pa5;
  * @version 1.0
  */
 public abstract class Constants {
-    // resources
+    // path
     private static final String PATH_RESOURCES = "/ffhs/pa5/resources";
 
-    public static final String FXML_STATISTICS = PATH_RESOURCES + "/statistics.fxml";
-    public static final String FXML_GAMEBOARD = PATH_RESOURCES + "/gameBoard.fxml";
-    public static final String FXML_START_MENU = PATH_RESOURCES + "/startMenu.fxml";
-    public static final String FXML_MAIN = FXML_START_MENU;
+    // fxml
+    public static final String FXML_MAIN = PATH_RESOURCES + "/application.fxml";
 
-    public static final String CSS_MAIN = PATH_RESOURCES + "/main.css";
-
-    public static final String IMAGE_SEED = PATH_RESOURCES + "/seed.png";
+    // css
+    public static final String CSS_MAIN = PATH_RESOURCES + "/application.css";
 
     // view
-    public static final String VIEW_TITLE = "Ouril";
-    public static final int VIEW_WIDTH = 1000;
-    public static final int VIEW_HEIGHT = 393;
-
-    // game-view
-    public static final int GAME_TURN_DELAY = 100;
+    public static final String VIEW_TITLE = "TODO";
+    public static final int VIEW_WIDTH = 600;
+    public static final int VIEW_HEIGHT = 500;
 }
