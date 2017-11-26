9bcae84218e384bb44cef757b32e6aa044f7ec54
Yves Riedener <git@reecube.com>
2017-11-26 18:54:57 +0100

    removed unused tests and added todo-test

---
+++ src/ffhs/pa5/tests/TodoTest.java
@@ -0,0 +1,23 @@
+package ffhs.pa5.tests;
+
+import org.junit.Test;
+
+/**
+ * TODO
+ *
+ * @author Tamara Burri
+ * @author Yves Riedener
+ * @version 1.0
+ */
+public class TodoTest {
+
+    /**
+     * TODO
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void testInitialize() throws Exception {
+        throw new Exception("Not implemented yet!");
+    }
+}
