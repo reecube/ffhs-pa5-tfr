4d25bcbd1e75889e61a4aaa7e3957587f117bed0
Yves Riedener <git@reecube.com>
2017-11-30 13:51:34 +0100

    implemented tests

--- src/ffhs/pa5/tests/ArchiveFactoryTest.java
+++ src/ffhs/pa5/tests/ArchiveFactoryTest.java
@@ -4,37 +4,78 @@ import ffhs.pa5.model.util.ArchiveFactoryEntry;
 import ffhs.pa5.util.ArchiveFactory;
 import org.junit.Test;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+
+import static org.junit.Assert.*;
 
 /**
- * TODO
+ * Tests for the ArchiveFactory class.
  *
  * @author Murat Tokmak
  * @author Barbara Fauth
  * @author Yves Riedener
  * @version 1.0
  */
-public class ArchiveFactoryTest {
+public class ArchiveFactoryTest extends TestingBase {
 
     /**
-     * TODO
-     *
-     * @throws Exception the exception
+     * Tests the ArchiveFactoryEntry class.
      */
     @Test
-    public void testEverything() throws Exception {
-        // TODO: improve this
-        ArchiveFactory archiveFactory = new ArchiveFactory("test.zip");
-        ArrayList<ArchiveFactoryEntry> entries = new ArrayList<>();
+    public void testArchiveFactoryEntry() {
 
-        entries.add(new ArchiveFactoryEntry("dir1", "file1.txt", "Test"));
+        // Config variables
+        final String directory = "sample-dir";
+        final String file = "sample.zip";
+        final String content = "lorem ipsum";
+        final String path = directory + File.separatorChar + file;
 
-        archiveFactory.write(entries.toArray(new ArchiveFactoryEntry[0]), true);
+        // Variable declaration
+        ArchiveFactoryEntry archiveFactoryEntry;
 
-        ArchiveFactoryEntry[] result = archiveFactory.read(true);
+        logTestCase("String input only");
+        archiveFactoryEntry = new ArchiveFactoryEntry(directory, file, content);
+        assertEquals(archiveFactoryEntry.getPath(), path);
+        assertEquals(archiveFactoryEntry.getContent(), content);
 
-        for (ArchiveFactoryEntry entry:result) {
-            System.out.println(entry);
+        logTestCase("ZipEntry and String input");
+        ZipEntry zipEntry = new ZipEntry(path);
+        archiveFactoryEntry = new ArchiveFactoryEntry(zipEntry, content);
+        assertEquals(archiveFactoryEntry.getPath(), path);
+        assertEquals(archiveFactoryEntry.getContent(), content);
+    }
+
+    /**
+     * Tests the ArchiveFactory lifecycle of an archive file.
+     */
+    @Test
+    public void testLifecycle() {
+
+        // Config variables
+        final String archiveFile = "test.zip";
+        final ArrayList<ArchiveFactoryEntry> entries = new ArrayList<>();
+        entries.add(new ArchiveFactoryEntry("dir1", "file1.txt", "Test11"));
+        entries.add(new ArchiveFactoryEntry("dir1", "file2.txt", "Test12"));
+        entries.add(new ArchiveFactoryEntry("dir2", "file1.txt", "Test21"));
+        final ArchiveFactoryEntry[] content = entries.toArray(new ArchiveFactoryEntry[0]);
+
+        // Variable declaration
+        ArchiveFactory archiveFactory;
+        ArchiveFactoryEntry[] result;
+
+        logTestCase("Write file");
+        archiveFactory = new ArchiveFactory(archiveFile);
+        assertTrue(archiveFactory.write(content, true));
+
+        logTestCase("Read file");
+        archiveFactory = new ArchiveFactory(archiveFile);
+        result = archiveFactory.read(true);
+        assertEquals(result.length, content.length);
+        for (int i = 0; i < result.length; i++) {
+            assertEquals(result[i].getPath(), content[i].getPath());
+            assertEquals(result[i].getContent(), content[i].getContent());
         }
     }
 }
---
+++ src/ffhs/pa5/tests/TestingBase.java
@@ -0,0 +1,29 @@
+package ffhs.pa5.tests;
+
+/**
+ * Testing base with helper functions for the testing environment.
+ *
+ * @author Murat Tokmak
+ * @author Barbara Fauth
+ * @author Yves Riedener
+ * @version 1.0
+ */
+abstract class TestingBase {
+
+    /**
+     * Shows the defined testing case in the console.
+     *
+     * @param testingCase  the testing case description
+     */
+    void logTestCase(String testingCase) {
+
+        // Get the parent function
+        final StackTraceElement[] ste = Thread.currentThread().getStackTrace();
+        final String testingGroup = ste[2].getMethodName();
+
+        String className = this.getClass().getSimpleName();
+
+        System.out.format("%s: %s: %s", className, testingGroup, testingCase);
+        System.out.println();
+    }
+}
