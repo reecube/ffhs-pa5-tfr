44f8d886901d77911cc5860956905da390134606
Yves Riedener <git@reecube.com>
2017-12-09 17:35:41 +0100

    removed unused code (this will be called in the view)

--- src/ffhs/pa5/controller/Controller.java
+++ src/ffhs/pa5/controller/Controller.java
@@ -1,17 +1,8 @@
 package ffhs.pa5.controller;
 
-import ffhs.pa5.Constants;
-import ffhs.pa5.util.Logger;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.*;
 
@@ -33,37 +24,7 @@ public class Controller implements Initializable {
     // ********************************************************************************
     // fxml components
     // ********************************************************************************
-
-    @FXML
-    private BorderPane main;
-
-    /**
-     * shows the different strategies
-     *
-     * @param event the event
-     */
-    @FXML
-    @SuppressWarnings({"unchecked", "unused"})
-    public void handleStart(ActionEvent event) {
-        try {
-
-            Node node = (Node) event.getSource();
-            this.stage = (Stage) node.getScene().getWindow();
-            FXMLLoader loader = new FXMLLoader(getClass().getResource(Constants.FXML_MAIN));
-            loader.setController(this);
-            this.main = loader.load();
-            Scene scene = new Scene(main, Constants.VIEW_WIDTH, Constants.VIEW_HEIGHT);
-            scene.getStylesheets().add(getClass().getResource(Constants.CSS_MAIN).toExternalForm());
-            stage.setScene(scene);
-
-            // TODO
-
-            stage.show();
-        } catch (Exception ex) {
-            final Logger logger = Logger.getInstance();
-            logger.handleException(ex);
-        }
-    }
+    // TODO
 
 
     // ********************************************************************************
